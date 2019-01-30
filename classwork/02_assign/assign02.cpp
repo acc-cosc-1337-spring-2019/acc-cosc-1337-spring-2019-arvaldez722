b16dfd2bbad2f0f702a8a9249f70ad4cb1e18116
April Valdez <april.valdez@g.austincc.edu>
2019-01-29 19:27:39 -0600

    Assignment 2 and junks?!?!?!

--- classwork/02_assign/assign02.cpp
+++ classwork/02_assign/assign02.cpp
@@ -1,1 +1,9 @@
 //Write the function code that returns the product of hours and hourly_rate.
+#include<iostream>
+#include"assign02.h"
+
+diuble grosspay(double hours, double hourly_rate)
+{
+	return
+
+}
--- classwork/02_assign/main.cpp
+++ classwork/02_assign/main.cpp
@@ -1,20 +1,36 @@
 //Write the include statement for assign02 here
+#include"assign02.h" 
 //Write include for capturing input from keyboard and displaying output to screen
+#include<iostream>
 
+using std::cout;
+using std::cin;
 int main() 
+
 {
 	//create a double variable named hours
 	//create a double variable named hourly_rate
+	double hours;
+	double hourly_rate;
 
 	//Prompt user for keyboard input for hours and hourly_rate
 	//save the input to variables hours and hourly_rate
 
+	cout<<"(Enter Hours: )";
+	cin >> hours;
+	cout << ("Enter Hourly Rate: ");
+	cin >> hourly_rate;
+
 	//Create a double variable named gross_salary
 	//Call the gross_pay function and save its return value to the gross_salary variable
+
+	double gross_salary;
+	gross_salary = grosspay(hours, hourly_rate);
 
 	//Display the gross_salary variable as follows:
 	//Example ---->>>     Gross pay: 150.00   
 
+	cout << "Gross pay is: " << gross_salary;
 
 	return 0;
 }
