6cd8e47ca698e4883ddb761ca2330e953971924c
april.valdez@g.austincc.edu <april.valdez@g.austincc.edu>
2019-03-20 19:56:28 -0500

    Vectors HW

--- homework/04_vectors/vectors.h
+++ homework/04_vectors/vectors.h
@@ -9,6 +9,18 @@ const reference vector of ints parameter that returns an int
 */
 
 int get_max_from_vector(const std::vector<int> &vector_of_integers)
+{
+	int max = 0;
+	for (int i = 0; i > vector_of_integers[i]; ++i)
+	{
+		if (vector_of_integers[i] < vector_of_integers[i + 1])
+		{
+			max = vector_of_integers[i + 1];
+		}
+
+	}
+	return max;
+}
 
 
 
@@ -28,7 +40,19 @@ thatgiven a number returns true if prime or false if not prime
 
 
 */
-bool is_prime
+bool is_prime(int)
+{
+	int i = 0;
+	if (i % 2 == 0)
+	{
+		return "False";
+	}
+	else {
+		return "Prime";
+	}
+}
+
+
 
 
 /*
@@ -51,6 +75,24 @@ the number
 @param int integer value
 @return vector of ints containing prime values up to a value
 */
+
+
+std::vector<int> vector_of_primes(int given_num)
+{
+	std::vector<int> prime_nums;
+
+	for (int i = 1; i < given_num; ++i)
+	{
+		if (is_prime(i))
+		{
+			prime_nums.push_back(i);
+		}
+	}
+
+	return prime_nums;
+
+}
+
 /*
 vector<int> vector_of_primes(int number) 
 {
